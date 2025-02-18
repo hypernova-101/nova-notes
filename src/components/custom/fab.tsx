aaf19a01ebd37e2e8c8415a0b85995fa9f85685c
hypernova-101 <hypernova1209gamer@gmail.com>
2025-02-19 00:31:12 +0530

    added upload dialog

--- src/components/custom/fab.tsx
+++ src/components/custom/fab.tsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
 import { Button } from "../ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogTitle, DialogTrigger } from "../ui/dialog";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
 
 export default function FloatingActionButton() {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [links, setlinks] = useState("")
     return( 
-        <Button className="">
-            Create
-        </Button>
+        <Dialog>
+            <DialogTrigger>
+                <Button className="">
+                    Create
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogTitle>Add Note</DialogTitle>
+                <Input
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Enter your title.."
+                    />
+                <Textarea
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    cols={100}
+                    rows={20}
+                    placeholder="Enter your content.."
+                    />
+                <Input
+                    value={links}
+                    onChange={(e) => setlinks(e.target.value)}
+                    placeholder="Enter your links.."
+                />
+                <DialogFooter>
+                    <Button>
+                        Save
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     )
 }
