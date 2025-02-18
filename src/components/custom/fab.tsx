12f48897835f9705d8f010d735eda2e9f0706044
hypernova-101 <hypernova1209gamer@gmail.com>
2025-02-19 00:20:55 +0530

    added firebase hosting

---
+++ src/components/custom/fab.tsx
@@ -0,0 +1,9 @@
+import { Button } from "../ui/button";
+
+export default function FloatingActionButton() {
+    return( 
+        <Button className="">
+            Create
+        </Button>
+    )
+}
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,7 +1,12 @@
+import FloatingActionButton from "@/components/custom/fab";
+
 export default function Layout() {
     return(
-        <nav className="p-4">
-            Nova Notes
+        <>
+        <nav className="flex flex-row justify-between items-center py-10 px-10 border-b-2">
+            <h1 className="text-4xl font-extrabold">Nova Notes</h1>
+        <FloatingActionButton/>
         </nav>
+        </>
     )
 }
