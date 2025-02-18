868565273fd4c8463092856995ac897d1668842a
hypernova-101 <hypernova1209gamer@gmail.com>
2025-02-18 23:57:25 +0530

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes } from "react-router-dom"
 import { Button } from "./components/ui/button"
+import AuthProvider from "./providers/auth-provider"
 
 function App() {
 
   return (
-    <>
+    <AuthProvider>
+      <BrowserRouter>
+      <Routes>
 
-    <Button>Thisis</Button>
-    </>
+        
+      </Routes>
+    </BrowserRouter>
+    </AuthProvider>
   )
 }
 
